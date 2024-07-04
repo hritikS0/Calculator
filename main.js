4f7cc1f85acd9a26db3d51e26189b22c2e09a8aa
hritikS0 <168188588+hritikS0@users.noreply.github.com>
2024-07-04 20:04:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Function to add characters to the display
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to evaluate the expression in the display
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
